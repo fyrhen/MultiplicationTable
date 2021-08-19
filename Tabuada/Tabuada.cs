8dd5980aab45dea2f58b04e417e52db16aa37e30
Fyrhen <julianogroberio@hotmail.com>
2021-08-19 09:22:57 -0300

    Add project files.

---
+++ Tabuada/Tabuada.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tabuada {
+    public class Tabuada {
+        
+        public static void Main() {
+            Console.WriteLine("Tabuada de 2");
+            for (int i =1; i<=10; i++) {
+                Console.WriteLine("2*{0}={1}", i, 2 * i);
+            }
+
+            Console.WriteLine("\r\nTabuada de 3");
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine("3*{0}={1}", i, 3 * i);
+            }
+
+            Console.WriteLine("\r\nTabuada de 5");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("5*{0}={1}", i, 5 * i);
+            }
+
+            Console.WriteLine("\r\nTabuada de 7");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("7*{0}={1}", i, 7 * i);
+            }
+
+            Console.WriteLine("\r\nTabuada de 10");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("10*{0}={1}", i, 10 * i);
+            }
+
+            Console.Read();
+        }
+    }
+}
